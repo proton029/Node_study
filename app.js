4b66f9f61b550d10afdfe8ca62b22df4062de7ae
Akhil Anand SM <akhil.anand@ilearningengines.com>
2023-02-20 16:34:30 +0530

    req and res in http

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const file = require('./filehandle')
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    res.write("dksdjd")
-    res.end();
+    if(req.url === '/about'){
+        res.end('<h1>About</h1>')
+    }
+    if(req.url === '/check'){
+        res.end('<h1>ABCD</h1>')
+    }
 })
 server.listen(5000)
