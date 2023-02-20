60dbc9945fd8666df2b400a22abfebbaa90fd190
Akhil Anand SM <akhil.anand@ilearningengines.com>
2023-02-20 16:21:52 +0530

    http

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-const {readFile} = require('fs')
 const file = require('./filehandle')
+const http = require('http')
 
-console.log('started a task');
-//check file path
-readFile('./content/read_file.txt','utf8',(err,result)=>{
-    if(err){
-        console.log(err)
-        return
-    }
-    console.log(result);
-    console.log("complicated task");
-
+const server = http.createServer((req,res)=>{
+    res.write("dksdjd")
+    res.end();
 })
-console.log("next task");
-//since readFile is Asynchronous and will offload the call back to the event loop so that it will not block 
-//other parallel process. so the first console will run then the last and once the reading is done
-// the 2nd console will get printed.
+server.listen(5000)
