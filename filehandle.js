740d9dcde01a35c2ad7423d9be161a515614a3a1
Akhil Anand SM <akhil.anand@ilearningengines.com>
2023-02-22 11:55:46 +0530

    using util

--- filehandle.js
+++ filehandle.js
@@ -1,38 +1,40 @@
-const {readFile} = require('fs')
+const { readFile, writeFile } = require("fs");
+const util = require("util");
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
-const getText = (path)=>{
-    return new Promise((resolve,reject) =>{
-        readFile(path,'utf8',(err,data)=>{
-            if(err){
-                reject(err);
-            }
-            else {
-                resolve(data);
-            }
-        })
-    })
-}
+//making cleaner
+
+const start = async () => {
+  try {
+    const first = await readFilePromise("./content/read_file.txt", "utf8");
+    const second = await readFilePromise("./content/write_file.txt", "utf8");
+    await writeFilePromise(
+      "./content/result-mind-grenade.txt",
+      `this is: ${first} and that is ${second}`
+    );
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
+
+module.exports = { start };
+
+// const getText = (path)=>{
+//     return new Promise((resolve,reject) =>{
+//         readFile(path,'utf8',(err,data)=>{
+//             if(err){
+//                 reject(err);
+//             }
+//             else {
+//                 resolve(data);
+//             }
+//         })
+//     })
+// }
 
 // getText('./content/read_file.txt')
 // .then((result)=>console.log(result))
 // .catch((err)=>console.log(err))
-
-//making cleaner
-
-const start=async()=>{
-    try{
-        const first = await getText('./content/read_file.txt')
-        const second = await getText('./content/write_file.txt')
-        console.log(first,second);
-    }catch(error){
-        console.log(error);
-    }
-    
-    
-}
-start()
-
-
-
-module.exports ={getText}
-
